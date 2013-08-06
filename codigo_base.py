9b0e112d0bd72acd8842824f686f854a87c90957
Erick Mendonça <erickmendonca@yahoo.com.br>
2013-08-06 19:57:38 -0300

    validando argumentos

--- codigo_base.py
+++ codigo_base.py
@@ -9,11 +9,23 @@
 import unittest
 
 def problema_miojo(tempo_miojo, amp_1, amp_2):
-    return True
+    if amp_1 < tempo_miojo or amp_2 < tempo_miojo or amp_1 == amp_2:
+    	return -1
+
+
+
+    return 0
+ # 0 - nao e possivel
+ # -1 - erro
+ # > 0 possivel
 
 class ProblemaMiojoTest(unittest.TestCase):
-    def test_simples(self):
-        self.assertEqual(True, problema_para_resolver())
+    def test_argumentos_1(self):
+        self.assertEqual(-1, problema_miojo(5, 3, 2))
+    def test_argumentos_2(self):
+    	self.assertEqual(-1, problema_miojo(5, 10, 10))
+    #def test_derradeiro(self)
+    #	self.assertEqual(-1, problema_miojo(5, 10, 10))
 
 if __name__ == '__main__':
     unittest.main()
