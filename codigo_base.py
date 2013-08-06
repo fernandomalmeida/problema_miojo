7958528671d6bc0f57fb961930611241c97c23e0
Erick Mendonça <erickmendonca@yahoo.com.br>
2013-08-06 20:20:38 -0300

    teste do site

--- codigo_base.py
+++ codigo_base.py
@@ -17,11 +17,12 @@ def problema_miojo(tempo_miojo, amp_1, amp_2):
     	diferenca = amp_2 - amp_1
     	maiortempo = amp_2
     else:
-    	diferenca = amp_1 - amp_2
-    	maiortempo = amp_1
+    	return problema_miojo(tempo_miojo,amp_2,amp_1)
 
     if diferenca == tempo_miojo:
     	return maiortempo
+    else:
+    	return problema_miojo(tempo_miojo, amp_1*2, amp_2)
 
     return 0
  # 0 - nao e possivel
@@ -33,8 +34,12 @@ class ProblemaMiojoTest(unittest.TestCase):
         self.assertEqual(-1, problema_miojo(5, 3, 2))
     def test_argumentos_2(self):
     	self.assertEqual(-1, problema_miojo(5, 10, 10))
-    def test_derradeiro(self):
+    def test_diferenca_simples(self):
     	self.assertEqual(7, problema_miojo(2, 5, 7))
+    def test_diferenca_trocada(self):
+    	self.assertEqual(7, problema_miojo(2, 7, 5))
+    def test_derradeiro(self):
+    	self.assertEqual(10, problema_miojo(3, 5, 7))	
 
 if __name__ == '__main__':
     unittest.main()
